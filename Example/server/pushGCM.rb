bc0617ed30d096e7d9cf39ab049d0c64addc26fd
Roman Beránek <beranek@ginasystem.com>
2015-03-02 09:12:12 +0100

    bad quotation marks

--- Example/server/pushGCM.rb
+++ Example/server/pushGCM.rb
@@ -4,6 +4,6 @@ GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
 GCM.key = "AIzaSyAnjIYGjWtSmLBmqZW6m6qbeUmdz8ceEho"
 destination = ["APA91bEFzmSQtT7TAIM5542WQQ3MC1fEQUTCFjWA5cZ0Inzny_u2FD6hOndbIEQYu1kO7nEi3ppNB2DunvcVigG5oiuZTF4VOMz7-JbPpExjo0LoWo5-XNPnkUupmyzC9uydz83FlMGGjzksnZitEiAfYGWg5nnieSJiJg_8feZgTFRWA-MpRjs"]
-data = {:type => “pkv”}
+data = {:type => "pkv"}
 
 GCM.send_notification( destination, data)
